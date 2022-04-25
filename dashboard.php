ad7ce01c55ab203ca7af236da1f44c92b6b72097
Alpha-coderoid <85844505+Alpha-coderoid@users.noreply.github.com>
2022-04-25 18:45:08 +0200

    error fixes and added behavior
    1- fixed an error where the function doesn't display the function if the amount of function was an even number
    2- fixed an error where the function would read a multi-digit number as multiple numbers
    3- linked the dashboard function to the login function and to HTML form

--- dashboard.php
+++ dashboard.php
@@ -1,77 +1,87 @@
 <?php
 include "UserFnc.php";
-include "Login.php";
+//include "Login.php";
 
 $fileName="UsersFile.txt";
 $fileName2="user type menu.txt";
 $fileName3="menu.txt";
+$pass=sha1($_REQUEST["Password"]);
 
-//ListAll($fileName);
-//$g=Login("ayman4@gmail.com",123);
-//echo $g;
-/*for($i=1;$i<6;$i++)
-   { $Id=$i;
-    $Record=getRowById($fileName,"~",$Id);
-    $ArrayResult=explode("~",$Record);
-	$Result['id']=$ArrayResult[0];
-    echo $ArrayResult[0];
-	$Result['Email']=$ArrayResult[1];
-    echo $ArrayResult[1];
-	$Result['Password']=$ArrayResult[2];
-    echo $ArrayResult[2];
-	$Result['FullName']=$ArrayResult[3];
-    echo $ArrayResult[3];
-	$Result['DOB']=$ArrayResult[4];
-    echo $ArrayResult[4];
-   }*/
-   
-
-echo "hi";
-if(Login("mohamed@yahoo.com",357)==true)
+//echo "hi";
+if (Login($_POST["Email"],$pass ))
 {
-    $Password=357;
-   $line= searchUser($fileName,"mohamed@yahoo.com"."~".$Password);
-   $ArrayResult=explode("~",$line);
-	$Result['id']=$ArrayResult[0]; //2
-    //echo $ArrayResult[0];
-    $id=$ArrayResult[0];
-    $Separator="~";
-  $w=  getRowById($fileName2,$Separator,$id);
-  //echo $w; //line
-  $i=1;
-  $i2=2;
+	//echo "Success";
+	session_start();
+	
+	$_SESSION["Email"]=$_POST["Email"];
+	//echo 	$_SESSION["Email"];
+	
 
-  //echo $w[$i2];
-  while($w[$i2+1]=="~") //cond
-  {
-  $ArrayResult=explode("~",$w);
-  $e=$w[$i2];
-  //echo $e;
-  $v=$ArrayResult[$i];
-  //echo $i;
-  //echo $v;
-  $w2=  getRowById($fileName3,$Separator,$v);
-  //echo $w2;
-  $ArrayResult=explode("~",$w2);
- // $v2=$ArrayResult[1];
-   echo $ArrayResult[1];
-    $i2+=2;
-    $i++;
+    if(Login($_POST["Email"],$pass)==true)
+    {
+      $Password=sha1( $_REQUEST["Password"]);
+      $line= searchUser($fileName,$_POST["Email"]."~".$Password);
+      $ArrayResult=explode("~",$line);
+	    $Result['id']=$ArrayResult[0]; //2
+       // echo $ArrayResult[5];
+        $id=intval( $ArrayResult[5]);
+      $Separator="~";
+     $w=  getRowById($fileName2,$Separator,$id);
+     //echo $w; //line
+     $i=1;
+      $i2=2;
+      $Array=explode("~",$w);
+     $trv=0;
+     while($trv!=count($Array)-2) //cond
+     {
+        // echo "i is "."$i "."$trv   ";
+         $trv++;
+            $ArrayResult=explode("~",$w);
+         
+         $v=$ArrayResult[$i];
+             //echo $i;
+         //echo $v;
+           $w2=  getRowById($fileName3,$Separator,$v);
+           //echo $w2;
+           $ArrayResult=explode("~",$w2);
+           // $v2=$ArrayResult[1];
+          // echo $ArrayResult;
+         echo  $ArrayResult[1];
+            $i2+=2;
+          $i++;
+          echo "<br>";
+     }
+    // echo $i-1;
+    //echo $Array[$i];
 
-  }
-  //echo $i2;
- $ArrayResult=explode("~",$w);
- $e=$w[$i2];
-   //echo $e;
-  $v=$ArrayResult[1];
-  //echo $v;
-  $Separator="~";
-  $w2=  getRowById($fileName3,$Separator,$e);
-  //echo $w2;
- $ArrayResult=explode("~",$w2);
- $v2=$ArrayResult[1];
-    echo $v2;
-   
+    $tt=intval( $Array[$i]);
+   // $to=substr($$tt,0,-1);
+   // $tt--;
+     //echo $tt[2];  
+    // echo strlen($Array[$i]);
+    // echo strlen($tt);
+     $w2=  getRowById($fileName3,$Separator,$tt);
+    // echo $w2;
+     $ArrayResult=explode("~",$w2);
+     echo  $ArrayResult[1];
+     
+     /*       //echo $i2;
+                $ArrayResult=explode("~",$w);
+        $e=$w[$i2];
+      //echo $e;
+      $v=$ArrayResult[1];
+      //echo $v;
+     $Separator="~";
+     $w2=  getRowById($fileName3,$Separator,$e);
+     //echo $w2;
+     $ArrayResult=explode("~",$w2);
+            $v2=$ArrayResult[1];*/
+    }// echo $v2;
+}
+else
+{
+	echo "False Login";
+}
    // $g= getAllUsersByKeyWord($KeyWord);
    // echo $g;
     //$y=getRowById($fileName2,$Separator,$id);
@@ -85,6 +95,6 @@ if(Login("mohamed@yahoo.com",357)==true)
    // $ArrayResult2=explode("~",$j);
   //  echo $ArrayResult2[1];
 
-}
+
 
 ?>
